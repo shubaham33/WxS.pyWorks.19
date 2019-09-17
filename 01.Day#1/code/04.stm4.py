618d00a77781a45652876811f9a4edef5747d2e6
Tapan Pandey <pandey.tapan@hotmail.com>
2019-09-17 23:11:46 +0530

    Solutions to 01

--- 01.Day#1/code/04.stm4.py
+++ 01.Day#1/code/04.stm4.py
@@ -2,6 +2,6 @@
 A program that reads a number between 1,000 and 999,999 from the user 
 and prints it with a comma separating the thousands
 '''
-num = input('Enter the number between 1000 and 999,999: ')
+num = int(input('Enter the number between 1000 and 999,999: '))
 # Using string slicing
-print(num[:-3] + ',' + num[-3:])
+print(f"{num:,}")
