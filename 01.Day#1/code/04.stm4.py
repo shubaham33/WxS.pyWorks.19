0ce4578545471c7f2771e1556700c313d5afcedc
Tapan Pandey <pandey.tapan@hotmail.com>
2019-09-18 09:26:46 +0530

    Minor change to Day.01.04.stm4

--- 01.Day#1/code/04.stm4.py
+++ 01.Day#1/code/04.stm4.py
@@ -1,7 +1,7 @@
-''' 
-A program that reads a number between 1,000 and 999,999 from the user 
+'''
+A program that reads a number between 1,000 and 999,999 from the user
 and prints it with a comma separating the thousands
 '''
 num = int(input('Enter the number between 1000 and 999,999: '))
-# Using string slicing
+# Using f - srings
 print(f"{num:,}")
